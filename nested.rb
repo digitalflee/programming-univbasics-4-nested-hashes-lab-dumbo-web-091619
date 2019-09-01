95f2d9650a7d08ee5e302242f8cd1b4318047579
Keenan Alves <keenan.alves@gmail.com>
2019-09-01 00:40:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,8 @@ def changing_alan
         }
      }
      
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     #alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
